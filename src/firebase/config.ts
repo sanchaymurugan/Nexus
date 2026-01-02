19e099963a4e8eb336920ccb9856613cdd708222
sanchay murugan <sanchaytheracer@gmail.com>
2026-01-02 17:44:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8949467362-115ea",
+  "appId": "1:845004930018:web:fc2fcc246f7455e11357bb",
+  "apiKey": "AIzaSyCVQy8xq0s9kdYSLhfVtdoAEA6A0lAtbdM",
+  "authDomain": "studio-8949467362-115ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "845004930018"
+};
